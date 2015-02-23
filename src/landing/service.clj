40c5ff344849c87ff99e3ad0c35e0310c6eb930a
Kirill Salykin <kirill.salykin@gmail.com>
2015-02-23 20:08:11 +0100

    changes port to 80

--- src/landing/service.clj
+++ src/landing/service.clj
@@ -45,5 +45,5 @@
               ;; Either :jetty, :immutant or :tomcat (see comments in project.clj)
               ::bootstrap/type :jetty
               ;;::bootstrap/host "localhost"
-              ::bootstrap/port 8080})
+              ::bootstrap/port 80})
 
