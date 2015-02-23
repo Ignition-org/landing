4cc4ae76a3b29b6fda6ad27f63b999445d91f54d
Kirill Salykin <kirill.salykin@gmail.com>
2015-02-23 20:18:58 +0100

    try to get port number from the env

--- src/landing/service.clj
+++ src/landing/service.clj
@@ -23,27 +23,29 @@
 
 ;; Consumed by landing.server/create-server
 ;; See bootstrap/default-interceptors for additional options you can configure
-(def service {:env :prod
-              ;; You can bring your own non-default interceptors. Make
-              ;; sure you include routing and set it up right for
-              ;; dev-mode. If you do, many other keys for configuring
-              ;; default interceptors will be ignored.
-              ;; :bootstrap/interceptors []
-              ::bootstrap/routes routes
+(def service
+  (let [port (Integer/parseInt (get (System/getenv) "PORT" "8080"))]
+    {:env :prod
+      ;; You can bring your own non-default interceptors. Make
+      ;; sure you include routing and set it up right for
+      ;; dev-mode. If you do, many other keys for configuring
+      ;; default interceptors will be ignored.
+      ;; :bootstrap/interceptors []
+      ::bootstrap/routes routes
 
-              ;; Uncomment next line to enable CORS support, add
-              ;; string(s) specifying scheme, host and port for
-              ;; allowed source(s):
-              ;;
-              ;; "http://localhost:8080"
-              ;;
-              ;;::bootstrap/allowed-origins ["scheme://host:port"]
+      ;; Uncomment next line to enable CORS support, add
+      ;; string(s) specifying scheme, host and port for
+      ;; allowed source(s):
+      ;;
+      ;; "http://localhost:8080"
+      ;;
+      ;;::bootstrap/allowed-origins ["scheme://host:port"]
 
-              ;; Root for resource interceptor that is available by default.
-              ::bootstrap/resource-path "/public"
+      ;; Root for resource interceptor that is available by default.
+      ::bootstrap/resource-path "/public"
 
-              ;; Either :jetty, :immutant or :tomcat (see comments in project.clj)
-              ::bootstrap/type :jetty
-              ;;::bootstrap/host "localhost"
-              ::bootstrap/port 8080})
+      ;; Either :jetty, :immutant or :tomcat (see comments in project.clj)
+      ::bootstrap/type :jetty
+      ;;::bootstrap/host "localhost"
+      ::bootstrap/port port}))
 
